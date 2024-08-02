f2dead894a496195f79f6febd4fcab85d09decf3
aniruddhamokashi21 <mokashianiruddha02@gmail.com>
2024-08-02 10:19:49 +0530

    Model trainer with pkl file

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -47,22 +47,21 @@ class ModelTrainer:
                 "Adaboost Classifier": AdaBoostRegressor()
             }
 
-            model_report : dict = evaluate_model(X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test, 
-                                                 models=models)
+            model_report : dict = evaluate_model(X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test, models=models)
 
             best_model_score = max(sorted(model_report.values()))
 
             best_model_name  = list(model_report.keys())[
                 list(model_report.values()).index(best_model_score)
-            ]
+            ] 
 
             best_model = models[best_model_name]
 
-            if best_model_score<0.6:
+            if best_model_score < 0.6:
                 raise CustomException("No best model found")
-            
             logging.info("Best found model on both training and testing dataset")
 
+                 
             save_object(
                 file_path=self.model_trainer_config.tained_model_file_path,
                 obj=best_model
--- src/utils.py
+++ src/utils.py
@@ -27,7 +27,7 @@ def evaluate_model(X_train, y_train, X_test, y_test, models):
         for i in range (len(list(models))):
             model = list(models.values())[i]
 
-            model.fit(X_train, y_train)
+            model.fit(X_train, y_train) 
 
             y_train_pred = model.predict(X_train)
             y_test_pred = model.predict(X_test)
@@ -35,7 +35,7 @@ def evaluate_model(X_train, y_train, X_test, y_test, models):
             train_model_score = r2_score(y_train, y_train_pred)
             test_model_score = r2_score(y_test, y_test_pred)
 
-            report[list(model.keys())[i]] = test_model_score
+            report[list(models.keys())[i]] = test_model_score
 
         return report
 
